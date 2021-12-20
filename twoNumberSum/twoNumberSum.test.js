ea1934e3c53782a59d8c6058a60d554db6295662
Dylan <heyitsdylan@gmail.com>
2021-12-19 21:25:02 -0800

    nlogn solution

--- twoNumberSum/twoNumberSum.test.js
+++ twoNumberSum/twoNumberSum.test.js
@@ -1,15 +1,22 @@
 const { test, expect } = require('@jest/globals');
 const twoNumberSum = require('./twoNumberSum');
 const twoNumberSum2 = require('./twoNumberSum2');
+const twoNumberSum3 = require('./twoNumberSum3');
 
-test('Two Number Sum', () => {
+test('Two Number Sum O(n2)', () => {
   const output = twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
   expect(output.length).toEqual(2);
   expect(output).toEqual([11, -1]);
 });
 
-test('Two Number Sum (Hash table)', () => {
+test('Two Number Sum O(n)', () => {
   const output = twoNumberSum2([-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], 163);
   expect(output.length).toEqual(2);
   expect(output).toEqual([210, -47]);
+})
+
+test('Two Number Sum O(nlogn)', () => {
+  const output = twoNumberSum3([-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], 163);
+  expect(output.length).toEqual(2);
+  expect(output).toEqual([-47, 210]);
 })
---
+++ twoNumberSum/twoNumberSum3.js
@@ -0,0 +1,18 @@
+const twoNumberSum3 = (array, targetSum) => {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    const currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left], array[right]];
+    } else if (currentSum < targetSum) {
+      left++;
+    } else if (currentSum > targetSum) {
+      right--;
+    }
+  }
+  return [];
+}
+
+module.exports = twoNumberSum3;
