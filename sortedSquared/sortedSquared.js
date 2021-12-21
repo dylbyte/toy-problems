d2673a222ecfb06483fed54d740e8dc5f2bd8aa6
Dylan <heyitsdylan@gmail.com>
2021-12-20 21:30:38 -0800

    Add eslint

--- sortedSquared/sortedSquared.js
+++ sortedSquared/sortedSquared.js
@@ -1,6 +1,6 @@
 const sortedSquared = (array) => {
-  const squares = array.map(num => num**2);
+  const squares = array.map((num) => num ** 2);
   return squares.sort((a, b) => a - b);
-}
+};
 
 module.exports = sortedSquared;
