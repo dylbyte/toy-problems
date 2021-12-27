98ce3c85a53e1b340bd1e7c61c55ef3092fca7bc
Dylan <heyitsdylan@gmail.com>
2021-12-27 13:24:35 -0800

    tournament winner solution

---
+++ tournamentWinner/tournamentWinner.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-param-reassign */
+// 3 points for winner
+function updateScores(scores, winner) {
+  if (!scores[winner]) scores[winner] = 0;
+  scores[winner] += 3;
+}
+
+function tournamentWinner(competitions, results) {
+  const HOME_TEAM_WON = 1;
+  const scores = {};
+  let bestTeam = '';
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < competitions.length; i++) {
+    const [homeTeam, awayTeam] = competitions[i];
+    let winningTeam = '';
+
+    if (results[i] === HOME_TEAM_WON) {
+      updateScores(scores, homeTeam);
+      winningTeam = homeTeam;
+    } else {
+      updateScores(scores, awayTeam);
+      winningTeam = awayTeam;
+    }
+
+    if (!bestTeam || scores[winningTeam] > scores[bestTeam]) {
+      bestTeam = winningTeam;
+    }
+  }
+  return bestTeam;
+}
+
+// Do not edit the line below.
+module.exports = tournamentWinner;
