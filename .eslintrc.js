6996f75cb220ba7f2daf95e35a07a546dd41b014
Dylan <heyitsdylan@gmail.com>
2021-12-27 16:45:17 -0800

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,19 @@
 module.exports = {
   env: {
-    browser: true,
+    // browser: true,
     commonjs: true,
     es2021: true,
+    node: true
   },
   extends: [
-    'airbnb-base',
+    "eslint:recommended",
   ],
   parserOptions: {
-    ecmaVersion: 13,
+    ecmaVersion: 2020,
   },
   rules: {
-  },
-};
+    "no-param-reassign": 2,
+    "no-plusplus": [2, { allowForLoopAfterthoughts: true }],
+    "quotes": 1
+  }
+}
