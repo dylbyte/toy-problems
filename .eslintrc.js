7349a11bcee2f5613fb990039be5eada826ce3f8
Dylan <heyitsdylan@gmail.com>
2022-01-04 22:18:51 -0800

    Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
   env: {
-    // browser: true,
     commonjs: true,
     es2021: true,
     node: true
   },
-  extends: [
-    "eslint:recommended",
-  ],
+  extends: "eslint:recommended",
   parserOptions: {
     ecmaVersion: 2020,
   },
@@ -15,6 +12,7 @@ module.exports = {
     "no-param-reassign": 1,
     "no-plusplus": [2, { allowForLoopAfterthoughts: true }],
     "quotes": 1,
-    "semi": 1
+    "semi": 1,
+    "no-multi-spaces": 1,
   }
-}
+};
