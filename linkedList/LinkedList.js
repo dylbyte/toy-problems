8318ef5f709de9fe262ccd92806e1032f6e98563
Dylan <heyitsdylan@gmail.com>
2022-01-12 18:55:53 -0800

    Partial LinkedList implementation

---
+++ linkedList/LinkedList.js
@@ -0,0 +1,31 @@
+class LinkedList {
+  constructor(value) {
+    this.head = value;
+    this.value = value;
+    this.next = null;
+  }
+
+  addMany(values) {
+    let current = this;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    for (const value of values) {
+      current.next = new LinkedList(value);
+      current = current.next;
+    }
+    return this;
+  }
+
+  getNodesInArray() {
+    const nodes = [];
+    let current = this;
+    while (current !== null) {
+      nodes.push(current.value);
+      current = current.next;
+    }
+    return nodes;
+  }
+}
+
+module.exports = LinkedList;
