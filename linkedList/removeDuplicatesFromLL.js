3c4fcc249da70edfd871e2ee90cc3d8c0792eec2
Dylan <heyitsdylan@gmail.com>
2022-01-12 23:22:16 -0800

    removeDuplicates solution

--- linkedList/removeDuplicatesFromLL.js
+++ linkedList/removeDuplicatesFromLL.js
@@ -1,4 +1,17 @@
-function removeDuplicatesFromLinkedList() {
+function removeDuplicatesFromLinkedList(linkedList) {
+  let currentNode = linkedList;
+	
+	while (currentNode.next) {
+		let next = currentNode.next;
+		
+		if (next.value === currentNode.value) {
+			// modify next pointer -> removes duplicate node
+			currentNode.next = next.next;
+		} else {
+			currentNode = currentNode.next;
+		}
+	}
+  return linkedList;
 
 }
 
