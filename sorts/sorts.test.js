c7bcabc55bd13c51c6bbc2eba60da225a5a65977
Dylan <heyitsdylan@gmail.com>
2022-01-29 16:50:33 -0800

    mergeSort test

--- sorts/sorts.test.js
+++ sorts/sorts.test.js
@@ -1,8 +1,13 @@
 const { describe, expect, test } = require("@jest/globals");
 const insertionSort = require("./insertionSort");
+const mergeSort = require("./mergeSort");
 
 describe("sorting tests", () => {
-  test("insertion sort - returns input array sorted", () => {
+  test("insertion sort works", () => {
     expect(insertionSort([55, 43, 11, -7, 1])).toEqual([-7, 1, 11, 43, 55]);
+  });
+
+  test("merge sort works", () => {
+    expect(mergeSort([-1, -2, -3, -7, -17, -27, -18, -541, -8, -7, 7]));
   });
 });
