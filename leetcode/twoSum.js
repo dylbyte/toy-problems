5267c16ce9ee5e401f329ac48881dc3347bdb3c8
Dylan <heyitsdylan@gmail.com>
2022-03-15 11:05:52 -0700

    twoSum working

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -1,3 +1,12 @@
-function twoSum(array1, array2) {}
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    let current = nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (current + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
 
 module.exports = twoSum;
