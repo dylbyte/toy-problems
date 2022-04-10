c756a2adc88dccaf00f49553ac89a76c7e52e0d7
Dylan <heyitsdylan@gmail.com>
2022-04-10 15:00:32 -0700

    valid parentheses

---
+++ leetcode/validParentheses.js
@@ -0,0 +1,16 @@
+/*
+ ** Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ ** An input string is valid if:
+ **  Open brackets must be closed by the same type of brackets.
+ **  Open brackets must be closed in the correct order.
+ */
+
+const validParentheses = (s) => {
+  // const stack = [];
+  // for (const char of s) {
+  //   stack.push(char);
+  // }
+  // return true;
+};
+
+module.exports = validParentheses;
