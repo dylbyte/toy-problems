f8c2656c171212289121a72f727cad2e22673fbe
Dylan <heyitsdylan@gmail.com>
2022-05-03 20:22:04 -0700

    max subarray linear solution

--- leetcode/maxSubarray2.js
+++ leetcode/maxSubarray2.js
@@ -1,3 +1,4 @@
+// O(n^2) Time
 const maxSubArray = (nums) => {
   let maxSum = nums[0];
   let currentSum = Number.MIN_VALUE;
@@ -12,4 +13,19 @@ const maxSubArray = (nums) => {
   return maxSum;
 };
 
-module.exports = maxSubArray;
+// O(n) Time
+const maxSubArray2 = (nums) => {
+  let maxSum = nums[0];
+  let currentSum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    currentSum = Math.max(currentSum + nums[i], nums[i]);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+  return maxSum;
+};
+
+module.exports = {
+  maxSubArray,
+  maxSubArray2,
+};
