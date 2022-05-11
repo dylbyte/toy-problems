f36a1bb6000daec8baed1d36e0af4dde34f1ba84
Dylan <heyitsdylan@gmail.com>
2022-05-11 12:46:05 -0700

    max subarray sum sliding window approach

--- patterns/sliding-window/maxSubarraySum.js
+++ patterns/sliding-window/maxSubarraySum.js
@@ -14,6 +14,31 @@ const maxSum = (nums, n) => {
   return max;
 };
 
+// O(n) T | sliding window approach
+const maxSum2 = (nums, n) => {
+  if (nums.length < n) return null;
+
+  let maxSum = 0;
+  let tempSum = 0;
+
+  // handles first n values
+  for (let i = 0; i < n; i++) {
+    maxSum += nums[i];
+  }
+
+  tempSum = maxSum;
+
+  // handles remainder of the array
+  for (let i = n; i < nums.length; i++) {
+    // subtract old window start, add new window end
+    tempSum = tempSum - nums[i - n] + nums[i]; // sliding the window
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+};
+
 module.exports = {
   maxSum,
+  maxSum2,
 };
