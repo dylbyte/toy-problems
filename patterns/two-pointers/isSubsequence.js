9ef666c728e672982feca34d0cb1d29acb39bf0c
Dylan <heyitsdylan@gmail.com>
2022-05-11 17:21:16 -0700

    isSubsequence recursive solution

--- patterns/two-pointers/isSubsequence.js
+++ patterns/two-pointers/isSubsequence.js
@@ -5,7 +5,7 @@
   Constraints: O(n + m) T | O(1) S
 */
 const isSubsequence = (s1, s2) => {
-  if (!s1) return false;
+  if (!s1) return true;
   let i = 0;
   let j = 0;
 
@@ -17,4 +17,15 @@ const isSubsequence = (s1, s2) => {
   return false;
 };
 
-module.exports = isSubsequence;
+// Recursive
+const isSubsequence2 = (s1, s2) => {
+  if (s1.length === 0) return true;
+  if (s2.length === 0) return false;
+  if (s2[0] === s1[0]) return isSubsequence2(s1.slice(1), s2.slice(1));
+  return isSubsequence2(s1, s2.slice(1));
+};
+
+module.exports = {
+  isSubsequence,
+  isSubsequence2,
+};
