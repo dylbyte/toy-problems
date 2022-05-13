75ceef01ac8a838bd707095141c9a75a4879e675
Dylan <heyitsdylan@gmail.com>
2022-05-13 15:55:29 -0700

    min size subarray sum alt solution

--- patterns/sliding-window/minSubarrayLen.js
+++ patterns/sliding-window/minSubarrayLen.js
@@ -11,6 +11,7 @@ const minSubarrayLen = (nums, target) => {
   let minLen = Number.MAX_SAFE_INTEGER;
   for (let i = 0; i < nums.length; i++) {
     let sum = nums[i];
+    if (sum >= target) return 1;
     let len = 1;
     for (let j = i + 1; j < nums.length; j++) {
       sum += nums[j];
@@ -25,27 +26,27 @@ const minSubarrayLen = (nums, target) => {
 };
 
 // O(n) T | O(1) S - sliding window approach
-const minSubarrayLen2 = (nums, sum) => {
+const minSubarrayLen2 = (nums, target) => {
   let total = 0;
   let start = 0;
   let end = 0;
   let minLen = Infinity;
 
   while (start < nums.length) {
-    // if current window doesnt add up to the given sum, move window to right
-    if (total < sum && end < nums.length) {
+    // if current window doesnt add up to the given target, move window to right
+    if (total < target && end < nums.length) {
       total += nums[end];
       end++;
     }
 
-    // if current window adds up to at least the sum given, shrink window
-    else if (total >= sum) {
+    // if current window adds up to at least the target given, shrink window
+    else if (total >= target) {
       minLen = Math.min(minLen, end - start);
       total -= nums[start];
       start++;
     }
 
-    // current total is less than required sum but we've reached the end
+    // current total is less than required target but we've reached the end
     else {
       break;
     }
@@ -53,7 +54,26 @@ const minSubarrayLen2 = (nums, sum) => {
   return minLen === Infinity ? 0 : minLen;
 };
 
+// O(n) T | O(1) S - alt. sliding window approach
+const minSubarrayLen3 = (nums, target) => {
+  let minLen = Number.MAX_SAFE_INTEGER;
+  let l = 0;
+  let sum = 0;
+  // left pointer & right pointer define window
+  // find smallest window that sums to target or greater
+  for (let r = 0; r < nums.length; r++) {
+    sum += nums[r];
+    while (sum >= target) {
+      minLen = Math.min(minLen, r - l + 1);
+      sum -= nums[l];
+      l++;
+    }
+  }
+  return minLen === Number.MAX_SAFE_INTEGER ? 0 : minLen;
+};
+
 module.exports = {
   minSubarrayLen,
   minSubarrayLen2,
+  minSubarrayLen3,
 };
