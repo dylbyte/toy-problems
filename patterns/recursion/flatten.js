19a9dedaf8d5bd1a190f89e860d297beacf4e671
Dylan <heyitsdylan@gmail.com>
2022-05-20 15:09:45 -0700

    alt flatten solution

--- patterns/recursion/flatten.js
+++ patterns/recursion/flatten.js
@@ -19,4 +19,19 @@ function flatten(arr) {
   return flattened;
 }
 
-module.exports = flatten;
+function flatten2(arr) {
+  let flattened = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flattened = flattened.concat(flatten2(arr[i]));
+    } else {
+      flattened.push(arr[i]);
+    }
+  }
+  return flattened;
+}
+
+module.exports = {
+  flatten,
+  flatten2,
+};
